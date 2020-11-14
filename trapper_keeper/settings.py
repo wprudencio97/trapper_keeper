28bee184f7c0c4581654c9aa8b5d02729584ffa4
William Prudencio <william@Williams-MacBook-Pro.local>
2020-11-14 15:21:56 -0500

    Fixed image upload error on richtext editor

--- trapper_keeper/settings.py
+++ trapper_keeper/settings.py
@@ -33,11 +33,12 @@ ALLOWED_HOSTS = ['django-trapper-keeper.herokuapp.com', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
-    'ckeditor_uploader',
     'notes',
+    'users',
     'storages',
     'crispy_forms',
     'ckeditor',
+    'ckeditor_uploader',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -145,18 +146,13 @@ CKEDITOR_CONFIGS = {
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
-AWS_S3_CUSTOM_DOMAIN = "django-blog-strg.s3.amazonaws.com"
-AWS_QUERYSTRING_AUTH = False
-
-
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
-
+#AWS_QUERYSTRING_AUTH = False
 
 CKEDITOR_UPLOAD_PATH = "uploads/"
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
